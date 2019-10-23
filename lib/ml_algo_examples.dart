66229020daf4eac32b701f0ff2cd40d9489d7587
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-10-24 00:49:14 +0300

    knn classification example added

--- lib/ml_algo_examples.dart
+++ lib/ml_algo_examples.dart
@@ -1,4 +1,5 @@
-import 'package:ml_algo_examples/src/classification/decision_tree.dart';
+import 'package:ml_algo_examples/src/classification/decision_tree_classification.dart';
+import 'package:ml_algo_examples/src/classification/knn_classification.dart';
 import 'package:ml_algo_examples/src/classification/logistic_regression.dart';
 import 'package:ml_algo_examples/src/classification/sofmax_regression.dart';
 import 'package:ml_algo_examples/src/regression/knn_regression.dart';
@@ -8,6 +9,7 @@ import 'package:ml_algo_examples/src/regression/stochastic_gradient_descent.dart
 void main() async {
   await decisionTreeRegression();
   await binaryClassification();
+  await knnClassification();
   await softmaxRegression();
   await knnRegression();
   await lassoRegression();
---
+++ lib/src/classification/knn_classification.dart
@@ -0,0 +1,39 @@
+import 'package:ml_algo/ml_algo.dart';
+import 'package:ml_dataframe/ml_dataframe.dart';
+import 'package:ml_preprocessing/ml_preprocessing.dart';
+
+Future knnClassification() async {
+  final samples = (await fromCsv('lib/_datasets/iris.csv'))
+      .dropSeries(seriesNames: ['Id']);
+
+  final pipeline = Pipeline(samples, [
+    encodeAsIntegerLabels(
+      featureNames: ['Species'],
+    ),
+  ]);
+
+  final processed = pipeline.process(samples);
+
+  final validator = CrossValidator.kFold(
+    processed,
+    ['Species'],
+    numberOfFolds: 7,
+  );
+
+  final accuracy = validator.evaluate(
+        (trainSamples, targetNames) =>
+        KnnClassifier(
+          trainSamples,
+          'Species',
+          5,
+        ),
+    MetricType.accuracy,
+  );
+
+  print('Iris dataset, KNN classifier, accuracy is '
+      '${accuracy.toStringAsFixed(2)}');
+}
+
+void main() async {
+  await knnClassification();
+}
