17cd592e9e8b0755dbbcca81ae6f0fc56dbd82c0
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-06-05 22:46:22 +0300

    pubspec updated

--- lib/src/regression/knn_regression.dart
+++ lib/src/regression/knn_regression.dart
@@ -1,5 +1,6 @@
 import 'package:ml_algo/ml_algo.dart';
 import 'package:ml_dataframe/ml_dataframe.dart';
+import 'package:ml_linalg/dtype.dart';
 import 'package:ml_preprocessing/ml_preprocessing.dart';
 
 Future knnRegression() async {
@@ -15,11 +16,12 @@ Future knnRegression() async {
     normalized,
     ['col_13'],
     numberOfFolds: folds,
+    dtype: DType.float64,
   );
 
   final error =
     validator.evaluate((trainSamples, targetNames) =>
-        KnnRegressor(trainSamples, targetNames.first, 4), MetricType.mape);
+        KnnRegressor(trainSamples, targetNames.first, folds), MetricType.mape);
 
   print('KNN regression on Boston housing dataset, label - `medv`, MAPE '
       'error on k-fold validation ($folds folds): '
