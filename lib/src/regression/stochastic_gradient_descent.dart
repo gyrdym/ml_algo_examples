0c9d8856418291a200c92231510b9a046f9571fd
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-06-18 23:12:38 +0300

    SGD regression example fixed

--- lib/src/regression/stochastic_gradient_descent.dart
+++ lib/src/regression/stochastic_gradient_descent.dart
@@ -1,34 +1,25 @@
 import 'package:ml_algo/ml_algo.dart';
 import 'package:ml_dataframe/ml_dataframe.dart';
-import 'package:ml_linalg/dtype.dart';
-import 'package:ml_preprocessing/ml_preprocessing.dart';
 
 Future sgdRegression() async {
   final samples = await fromCsv('lib/_datasets/housing.csv',
       headerExists: false,
       columnDelimiter: ' ');
-
-//  final normalized = Normalizer().process(samples);
-
   final folds = 5;
-
   final validator = CrossValidator.kFold(
     samples,
     ['col_13'],
     numberOfFolds: folds,
   );
-
   final error =
     validator.evaluate((trainSamples, targetNames) =>
         LinearRegressor(
             trainSamples,
             targetNames.first,
             optimizerType: LinearOptimizerType.gradient,
-            iterationsLimit: 1000,
-            initialLearningRate: 0.05,
-            minCoefficientsUpdate: null,
-            learningRateType: LearningRateType.decreasingAdaptive,
-            dtype: DType.float64),
+            initialLearningRate: 0.00000385,
+            randomSeed: 2,
+            learningRateType: LearningRateType.decreasingAdaptive),
         MetricType.mape);
 
   print('SGD regression on Boston housing dataset, label - `medv`, MAPE '
