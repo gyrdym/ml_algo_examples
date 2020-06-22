1c137b02320338e774396d9727ed1d1dd1aded30
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-06-23 00:12:48 +0300

    ml_algo v14.1.0

--- lib/src/classification/logistic_regression.dart
+++ lib/src/classification/logistic_regression.dart
@@ -3,30 +3,41 @@ import 'package:ml_dataframe/ml_dataframe.dart';
 
 Future logisticRegression() async {
   final samples = await fromCsv('lib/_datasets/pima_indians_diabetes_database.csv');
+  final splits = splitData(samples, [0.7]);
+  final validationData = splits[0];
+  final testData = splits[1];
   final numberOfFolds = 5;
+  final targetNames = ['class variable (0 or 1)'];
   final validator = CrossValidator.kFold(
-    samples,
-    ['class variable (0 or 1)'],
+    validationData,
+    targetNames,
     numberOfFolds: numberOfFolds,
   );
-  final logisticRegressorFactory = (trainSamples, targetNames) =>
+  final createClassifier = (DataFrame trainSamples, targetNames) =>
     LogisticRegressor(
       trainSamples,
       targetNames.first,
-      learningRateType: LearningRateType.decreasingAdaptive,
-      probabilityThreshold: 0.7,
       fitIntercept: true,
-      interceptScale: .1,
-      randomSeed: 3,
+      interceptScale: 200,
+      learningRateType: LearningRateType.decreasingAdaptive,
+      batchSize: trainSamples.rows.length,
     );
   final scores = await validator.evaluate(
-    logisticRegressorFactory,
+    createClassifier,
     MetricType.accuracy,
   );
 
-  print('Pima indians diabetes dataset, logistic regression: accuracy on '
-      'k-fold validation ($numberOfFolds folds): '
+  print('Pima indians diabetes dataset, Logistic regression');
+
+  print('Accuracy on k-fold validation ($numberOfFolds folds): '
       '${scores.mean().toStringAsFixed(2)}');
+
+  final testSplits = splitData(testData, [0.8]);
+  final classifier = createClassifier(testSplits[0], targetNames);
+  final finalScore = classifier.assess(testSplits[1], targetNames,
+      MetricType.accuracy);
+
+  print('Accuracy on the model assessment: ${finalScore.toStringAsFixed(2)}');
 }
 
 Future main() async {
